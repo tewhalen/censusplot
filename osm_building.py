04ebee97b501b28b69f4cdab60cdb669f50abbe2
ted whalen <tewhalen@gmail.com>
2017-11-16 15:35:22 -0600

    use prefix instead of direction

--- osm_building.py
+++ osm_building.py
@@ -4,7 +4,7 @@ import overpy
 def query_buildings(area, buildings):
     """Query OSM for buildings in the proper area, and get all their nodes.
 
-    buildings = [(number, direction, street),]"""
+    buildings = [(number, prefix, street),]"""
     q = """
     [timeout:25];
     area[name="{}"];
@@ -16,8 +16,8 @@ def query_buildings(area, buildings):
     """
     query_format = 'way["addr:housenumber"="{}"]["addr:street:name"="{}"]["addr:street:prefix"="{}"];'
     queries = []
-    for number, direction, street in buildings:
-        queries.append(query_format.format(number, street, direction))
+    for number, prefix, street in buildings:
+        queries.append(query_format.format(number, street, prefix))
 
     api = overpy.Overpass()
     return api.query(q.format(area, "\n".join(queries)))
@@ -25,10 +25,14 @@ def query_buildings(area, buildings):
 
 def main():
     nodes = query_buildings("Chicago",[(1917, "West","Berwyn"),
-                            (1911,"West","Berwyn")]).ways[0].get_nodes()
+                            (1911,"West","Berwyn"),
+                            (1907,"West","Berwyn"),
+                            (1916,"West","Berwyn")])
 
-    for n in nodes:
-        print((float(n.lat), float(n.lon)))
+    for w in nodes.ways:
+        print(w.tags)
+        for n in w.get_nodes():
+            print((float(n.lat), float(n.lon)))
 
 if __name__ == '__main__':
     main()
